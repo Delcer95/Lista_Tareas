61540ba1723bc28539e34361f393abb08f68afda
Delcer Hernández <156735968+Delcer95@users.noreply.github.com>
2025-03-19 18:56:17 -0600

    Create models.py

---
+++ models.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Tarea(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    descripcion = db.Column(db.String(200), nullable=False)
+    completada = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f'<Tarea {self.descripcion}>'
